f23488f39604a6b8a4942f71168926089de18e90
hoang <hoangtu2378@gmail.com>
2021-01-27 11:17:58 +0700

    tu

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    int id;
+    String name;
+    int age;
+
+    void run(){
+        eat();
+        System.out.printf(name + " Running... ");
+    }
+    void eat(){
+        System.out.printf(name + " eating... ");
+    }
+}
